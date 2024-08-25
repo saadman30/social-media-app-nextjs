72e972bfc381af5753abc2e2388f1c993d41a189
saadman30 <saadman.ofc@gmail.com>
2024-08-25 23:26:47 +0600

    initial commit

---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,63 @@
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { redirect } from "next/navigation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import prisma from "@/lib/prisma";
+
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
+
+    const passwordHash = await hash(password, {
+      memoryCost: 1945,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    const userId = generateIdFromEntropySize(10);
+
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return { error: "Username already taken" };
+    }
+
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return { error: "Email already taken" };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    redirect("/");
+  } catch (error) {
+    console.log(error);
+    return { error: "An error occurred while signing up" };
+  }
+}
